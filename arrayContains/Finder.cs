7b567fff33a2db68b259599f58cee56ff9cb8e45
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-09-19 11:44:15 +0300

    !

---
+++ arrayContains/Finder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace arrayContains
+{
+    internal class Finder
+    {
+        public static bool FindContains(string[] array,string whatFind)
+        {
+            int findCount = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Equals( whatFind)) findCount++;
+                
+            }
+            if (findCount == 0) return false;
+            else return true;
+        }
+    }
+}
---
+++ arrayContains/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace arrayContains
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myList = new string[2];
+            string a = "Yay!";
+            string b = "Bummer";
+            myList[0] = a;
+            myList[1] = b;
+            Console.WriteLine(myList.Length);
+            Console.WriteLine(Finder.FindContains(myList, b));
+            Console.ReadKey();
+        }
+    }
+}
