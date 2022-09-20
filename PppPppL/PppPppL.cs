cabd44d9b4477f3cd6f24e712a93318aa1ed1c72
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-09-20 08:05:43 +0300

    ppl done

---
+++ PppPppL/PppPppL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PppPppL
+{
+   public static class PppPppL
+    {
+        public static void PppPppLl(List<string> a)
+        {
+            string zilch = "zero";
+            string first = "one";
+            string second = "two";
+            string third = "three";
+            string fourth = "4.2";
+            string twopointtwo = "2.2";
+
+            a.Add(zilch);
+            a.Add(first);
+            a.Add(second);
+            a.Add(third);
+            a.RemoveAt(2);
+
+            if (a.Contains("three"))
+            {
+                a.Add("four");
+            }
+            if (a.IndexOf("four") != 4)
+            {
+                a.Add(fourth);
+            }
+            if (a.Contains("two"))
+            {
+                a.Add(twopointtwo);
+            }
+            foreach (string element in a)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
---
+++ PppPppL/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace PppPppL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> a = new List<string>();
+            PppPppL.PppPppLl(a);
+            
+
+            Console.ReadKey();
+           
+           
+            
+
+            
+
+        }
+    }
+}
