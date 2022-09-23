744a9514169657ca0a199ecd9f60e4c4c3ee622a
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-09-23 09:43:04 +0300

    EnumerableDuck done

--- EnumerableBird/Program.cs
+++ EnumerableBird/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EnumerableBird
 {
@@ -6,7 +8,18 @@ namespace EnumerableBird
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Duck> ducks = new List<Duck>()
+           {
+               new Duck() { Kind = KindOfDuck.Mallard, Size = 17 },
+               new Duck() { Kind = KindOfDuck.Muscovy, Size = 18 },
+               new Duck() { Kind = KindOfDuck.Loon,Size = 14 },
+               new Duck() { Kind = KindOfDuck.Muscovy, Size = 11 },
+               new Duck() { Kind = KindOfDuck.Mallard, Size = 14 },
+               new Duck() { Kind = KindOfDuck.Loon, Size = 13},
+           };
+            IEnumerable<Bird> upcastDucks = ducks;
+            Bird.FlyAway(upcastDucks.ToList(), "Minnesota");
+            Console.ReadLine();
         }
     }
 }
