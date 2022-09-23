008b8cd97df1a957ef2093b18668a1269fa68b77
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-09-23 10:48:30 +0300

    Done

---
+++ DictionaryApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> favoriteFoods = new Dictionary<string, string>();
+            favoriteFoods["Alex"] = "hot dogs";
+            favoriteFoods["A'ja"] = "pizza";
+            favoriteFoods["Jules"] = "falafel";
+            favoriteFoods["Naima"] = "spaghetti";
+            /*В словарь добавляются четыре пары «ключ / значение». В данном случае ключом является имя, 
+            а значением — любимая еда этого человека.*/
+            string name;
+            while ((name = Console.ReadLine()) != "");
+            {
+                if (favoriteFoods.ContainsKey(name)) //Метод ContainsKey возвращает true, если словарь содержит значение с указанным ключом.
+                    Console.WriteLine($"{name}'s favorite food is {favoriteFoods[name]}"); // получаем значение, связанное с ключом.
+                else
+                    Console.WriteLine($"I don't know {name}'s favorite food");
+            }
+        }
+    }
+}
