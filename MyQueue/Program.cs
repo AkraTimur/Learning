10f91ceda7826a1565a973f71bef1f55c5e6d451
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-10-05 19:16:59 +0300

    done

---
+++ MyQueue/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Создание очереди с добавлением четырех элементов
+        Queue<string> myQueue = new Queue<string>();
+        myQueue.Enqueue("first in line");
+        myQueue.Enqueue("second in line");
+        myQueue.Enqueue("third in line");
+        myQueue.Enqueue("last in line");
+
+        // Метод Peek "подсматривает" первый элемент очереди и возвращает его без удаления
+        Console.WriteLine($"Peek() returned:\n{myQueue.Peek()}");
+
+        // Метод Dequeue извлекает следующий элемент от НАЧАЛА очереди
+        Console.WriteLine($"The first Dequeue() returned:\n{myQueue.Dequeue()}");
+        Console.WriteLine($"The second Dequeue() returned:\n{myQueue.Dequeue()}");
+
+        // Clear удаляет все элементы из очереди
+        Console.WriteLine($"Count before Clear():\n{myQueue.Count}");
+        myQueue.Clear();
+        Console.WriteLine($"Count after Clear():\n{myQueue.Count}");
+    }
+}
