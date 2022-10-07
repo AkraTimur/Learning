039c9dee62ccf47565b5fc9e7e0a5517e3bc326a
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-10-07 10:12:49 +0300

    lumperjacks part one done

---
+++ Lumberjacks/Lumberjack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lumberjacks
+{
+    internal class Lumberjack
+    {
+        public string Name { get; set; }
+        public Lumberjack(string name)
+        {
+            Name = name;
+        }
+        private Stack<Flapjack> flapjackStack = new Stack<Flapjack>();
+        public void TakeFlapjack(Flapjack flapjack)
+        {
+            flapjackStack.Push(flapjack);
+        }
+
+        public void EatFlapjacks()
+        {
+
+            Console.WriteLine($"{Name} eat a {flapjackStack.Peek()} flapjacks");
+        }
+
+    }
+}
