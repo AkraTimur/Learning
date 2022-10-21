2f070af3155e96722d6d7620f9c78537bc506b69
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-10-21 10:01:54 +0300

    add new program

---
+++ WhatAmI/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace WhatAmI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var whatAmI = new {Color = "Blue", Flavor = "Tasty", Height = 37 }; // анонимный тип
+            Console.WriteLine(whatAmI);
+        }
+    }
+}
