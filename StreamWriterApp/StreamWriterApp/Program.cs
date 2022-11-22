a9dbd2dacc4863af69094ec0d5dc18dd1d63d908
AKRA <99009898+AkraTimur@users.noreply.github.com>
2022-11-22 17:07:58 +0300

    add new apps

---
+++ StreamWriterApp/StreamWriterApp/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace StreamWriterApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Flobbo f = new Flobbo("blue yellow");
+            StreamWriter sw = f.Snobbo();
+            f.Blobbo(f.Blobbo(f.Blobbo(sw), sw), sw);
+        }
+        class Flobbo
+        {
+            private string zap;
+            public Flobbo(string zap)
+            {
+                this.zap = zap;
+            }
+            public StreamWriter Snobbo()
+            {
+                return new StreamWriter("C:\\CSProjects\\learning\\StreamWriterApp\\macaw.txt");
+            }
+            public bool Blobbo(bool Already, StreamWriter sw)
+            {
+                if (Already)
+                {
+                    sw.WriteLine(zap); 
+                    sw.Close(); 
+                    return false;
+                }
+                else
+                {
+                    sw.WriteLine(zap);
+                    zap = "red orange";
+                    return true;
+                }
+            }
+
+            public bool Blobbo(StreamWriter sw)
+            {
+                sw.WriteLine(zap);
+                zap = "green purple";
+                return false;
+            }
+        }
+    }
+}
